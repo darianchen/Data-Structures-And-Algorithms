821deb3f1da5ccf7a35f3a162cc5beb548a6f860
Darian Chen <61950431+darianchen@users.noreply.github.com>
2022-12-01 15:39:14 -0500

    Time: 119 ms (37.65%), Space: 44.9 MB (39.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,17 +4,18 @@
  */
 var majorityElement = function(nums) {
  
-    let hash = {};
+//     let hash = {};
     
-    for(let i = 0; i < nums.length; i++){
-        if(hash[nums[i]]){ //if exist
-            hash[nums[i]]++;
-        }else{ //if not exist
-            hash[nums[i]] = 1;
-        }
-    }
+//     for(let i = 0; i < nums.length; i++){
+//         if(hash[nums[i]]){ //if exist
+//             hash[nums[i]]++;
+//         }else{ //if not exist
+//             hash[nums[i]] = 1;
+//         }
+//     }
     
-    return Object.keys(hash).find(key => hash[key] === Math.max(...Object.values(hash)))
+//     //return Object.keys(hash).find(key => hash[key] === Math.max(...Object.values(hash)))
     
+    return nums.sort()[Math.floor(nums.length/2)]
     
 };
