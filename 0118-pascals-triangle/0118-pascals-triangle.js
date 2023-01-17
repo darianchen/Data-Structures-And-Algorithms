8aac7fa1ce1ff302a2e33c838fd4ed852f5d8879
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-01-17 17:07:14 -0500

    Time: 72 ms (52.97%), Space: 42.6 MB (11.19%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,21 +3,40 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
+    
+    
+    let ans = [[1]];
+
+    while (ans.length < numRows) {
+        let last = ans[ans.length - 1];
+        let newRow = [1];
+
+        for (let i = 0; i < last.length - 1; i++) {
+            newRow.push(last[i] + last[i+1])
+        }
+        newRow.push(1);
+        ans.push(newRow);
+    }
+    return ans;
+    
+    
+    
+    
     // let ans = [[1]];
     // if(numRows === 1) return ans;
     
-    let ans = [[1]];
+//     let ans = [[1]];
     
-    for(let i = 1; i < numRows; i++){
-        let row = [1]
-        for(let j = 1; j < i; j++){
-            //addition
-            row[j] = ans[i-1][j] + ans[i-1][j-1];
+//     for(let i = 1; i < numRows; i++){
+//         let row = [1]
+//         for(let j = 1; j < i; j++){
+//             //addition
+//             row[j] = ans[i-1][j] + ans[i-1][j-1];
             
-        }
-        row.push(1);
-        ans.push(row);
-    }    
+//         }
+//         row.push(1);
+//         ans.push(row);
+//     }    
     
-    return ans;
+//     return ans;
 };
