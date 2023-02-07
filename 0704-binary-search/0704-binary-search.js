186fea185b58874e3670b9ca1d2db62fbe62cc4d
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-02-07 17:41:54 -0500

    Time: 67 ms (76.86%), Space: 47.1 MB (6.90%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,18 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-        if (nums.length === 0) return -1;
-
+    
+    if(nums.length === 0) return -1;
+    
     let midIdx = Math.floor(nums.length/2);
-    let left = nums.slice(0,midIdx);
+    let left = nums.slice(0, midIdx);
     let right = nums.slice(midIdx + 1);
-
+    
     if(nums[midIdx] === target){
         return midIdx;
-    }else if(nums[midIdx] > target){
+    } else if (nums[midIdx] > target){
         return search(left, target);
-    } else {
+    }else {
         let result = search(right, target);
-        return result === -1 ? -1 : midIdx + result + 1;
+        return result === -1 ? -1 : midIdx + 1 + result
     }
+    
 };
