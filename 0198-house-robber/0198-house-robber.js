2ff101c908b991521544bf4fd6d52ac59036cb59
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-02-09 18:27:00 -0500

    Time: 63 ms (66.48%), Space: 42.4 MB (9.62%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,64 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var rob = function(nums){
+    if(nums.length < 2) return nums[0];
+    
+    let memo = [nums[0], Math.max(nums[0], nums[1])];
+    let length = nums.length;
+    
+    for(let i = 2; i < length; i++){
+        let doRob = nums[i] + memo[memo.length - 2];
+        let dontRob = memo[memo.length - 1]; 
+        
+        memo.push(Math.max(doRob, dontRob));
+    }
+
+    
+    return Math.max(...memo);
+    
+};
+
+
+
+// var rob = function(nums) {
+//     let answer;    
+//     if(nums.length === 1){
+//         answer = nums[0]
+//     } else
+    
+//     if(nums.length === 2){
+//         answer = Math.max(...nums);
+//     } else{
+        
+//     //[2,4,1]; doRob = 3, dontRob = 4
+//     //[2,4,3]; doRob = 5, dontRob = 4
+    
+//     //Determine the amount if you do rob house i
+//     //then determine the amount if you dont rob house i
+//     //compare the 2 and save the big money
+    
+//     // for(let i = 0; i < nums.length; i++){
+    
+    
+//         let i = nums.length - 1;
+//         let doRob = nums[i] + rob(nums.slice(0,i - 1));
+//         let dontRob = rob(nums.slice(0, i));
+        
+//         if(doRob > dontRob){
+//             answer = doRob;
+//         } else{
+//             answer = dontRob;
+//         }
+        
+//     }
+        
+//         console.log(nums.length - 1, answer);
+        
+//         return answer;
+    
+//     // }
+    
+// };
