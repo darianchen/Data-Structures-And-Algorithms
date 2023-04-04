d9474e1ec5808d76b2798eeea9ad13ad48736588
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-03 20:12:17 -0400

    Time: 64 ms (84.48%), Space: 48 MB (16.43%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,17 +3,28 @@
  * @return {number}
  */
 function singleNumber(nums) {
-    const ht = {};
+
+  const set = new Set();
+  for (const num of nums) {
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  }
+  return set.values().next().value;
+
+//     const ht = {};
     
-    for(const num of nums) {
-        ht[num] = ht[num] + 1 || 1;
-    };
+//     for(const num of nums) {
+//         ht[num] = ht[num] + 1 || 1;
+//     };
     
-    for(const key in ht) {
-        if(ht[key] === 1) return key;
-    };
+//     for(const key in ht) {
+//         if(ht[key] === 1) return key;
+//     };
         
-    return ht;
+//     return ht;
     
   // let result = 0;
   // for (let num of nums) {
