f49506639663f9e10639a781dee5fd5280867e70
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-05 14:32:38 -0400

    Time: 59 ms (81.94%), Space: 42.2 MB (75.14%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,20 +3,33 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let prefix = [];
-    let bool = true;
-    for (let i = 0; i < strs[0].length; i++) {
-        let char = strs[0][i]
-        strs.forEach( (word)=> {
-            let letter = word[i];
-            if( (!letter) || (letter !== char) ){ bool = false; }
-        });
-        if(!bool){
-            return prefix.join('');
-        } else {
+    let prefix = "";
+    if(strs.length === 0) return prefix;
+    
+    for(let i = 0; i < strs[0].length; i++) {
+        let char = strs[0][i];
+        
+        for(let j = 1; j < strs.length; j++) {
+            if(char !== strs[j][i]) return prefix;
+        };  
+        prefix += char;
+    };
+    return prefix;
+    
+//     let prefix = [];
+//     let bool = true;
+//     for (let i = 0; i < strs[0].length; i++) {
+//         let char = strs[0][i]
+//         strs.forEach( (word)=> {
+//             let letter = word[i];
+//             if( (!letter) || (letter !== char) ){ bool = false; }
+//         });
+//         if(!bool){
+//             return prefix.join('');
+//         } else {
 
-            prefix.push(char);
-        }
-    }
-    return prefix.join('')
+//             prefix.push(char);
+//         }
+//     }
+//     return prefix.join('')
 };
