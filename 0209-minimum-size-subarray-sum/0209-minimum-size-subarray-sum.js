deb7c9804dfb42504ca563bb8cb6f12d2183ff26
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-10 18:21:21 -0400

    Time: 58 ms (94.51%), Space: 51.9 MB (6.51%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+      let minLen = Infinity;
+  let left = 0;
+  let sum = 0;
+
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+
+    while (sum >= target) {
+      const len = right - left + 1;
+      if (len < minLen) {
+        minLen = len;
+        subArray = nums.slice(left, right + 1);
+      }
+
+      sum -= nums[left];
+      left++;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+}
