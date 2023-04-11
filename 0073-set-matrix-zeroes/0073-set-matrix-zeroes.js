685e38f70e42aaa7ea92796d33153b9371648432
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-11 15:57:52 -0400

    Time: 79 ms (72.62%), Space: 44.5 MB (88.63%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -3,27 +3,47 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    let newZeros = new Set();
+    let rowSet = new Set();
+    let colSet = new Set();
     
     for(let row = 0; row < matrix.length; row++) {
         for(let col = 0; col < matrix[row].length; col++) {
-            if(matrix[row][col] === 0) newZeros.add(`${row},${col}`)
+            if(matrix[row][col] === 0) {
+                rowSet.add(row);
+                colSet.add(col);
+            }
+        }    
+    }
+
+    for(let row = 0; row < matrix.length; row++) {
+        for(let col = 0; col < matrix[row].length; col++) {
+            if(rowSet.has(row) || colSet.has(col)) matrix[row][col] = 0;
         }
     }
-    let zeroArr = Array.from(newZeros);
     
-    for(let i = 0; i < zeroArr.length; i++) {
-        let row = parseInt(zeroArr[i].split(',')[0]);
-        let col = parseInt(zeroArr[i].split(',')[1])
-        for(let pointer = 0; pointer < matrix[row].length; pointer++) {
-            matrix[row][pointer] = 0;
-        }
+// This works    
+//     let newZeros = new Set();
+    
+//     for(let row = 0; row < matrix.length; row++) {
+//         for(let col = 0; col < matrix[row].length; col++) {
+//             if(matrix[row][col] === 0) newZeros.add(`${row},${col}`)
+//         }
+//     }
+//     let zeroArr = Array.from(newZeros);
+    
+//     for(let i = 0; i < zeroArr.length; i++) {
+//         let row = parseInt(zeroArr[i].split(',')[0]);
+//         let col = parseInt(zeroArr[i].split(',')[1])
+//         for(let pointer = 0; pointer < matrix[row].length; pointer++) {
+//             matrix[row][pointer] = 0;
+//         }
         
-        for(let pointer = 0; pointer < matrix.length; pointer++) {
-            matrix[pointer][col] = 0;
-        }
-    }    
+//         for(let pointer = 0; pointer < matrix.length; pointer++) {
+//             matrix[pointer][col] = 0;
+//         }
+//     }    
     
+///////////////////////
     
 //     for(let row = 0; row < matrix.length; row++) {
 //         for(let col = 0; col < matrix[row].length; col++) {
