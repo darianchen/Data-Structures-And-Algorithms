1ef6a5a28c0007432371118f87f62c7fb1feb3b2
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-11 18:35:36 -0400

    Time: 58 ms (82.67%), Space: 42.8 MB (45.63%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.js
+++ 0682-baseball-game/0682-baseball-game.js
@@ -3,21 +3,41 @@
  * @return {number}
  */
 var calPoints = function(operations) {
-    
     let record = [];
-    
-    for(let i = 0; i < operations.length; i++) {
-        if(!isNaN(parseInt(operations[i]))) {
-            record.push(parseInt(operations[i]));
-        } else if (operations[i] === '+') {
-            let sum = record[record.length - 1] + record[record.length - 2];
-            record.push(sum);
-        } else if (operations[i] === 'D') {
-            record.push(record[record.length - 1] * 2);
-        } else {
-            record.pop();
+      
+    for(const op of operations) {
+        switch(op) {
+            case '+':
+                record.push(record.at(-1) + record.at(-2));
+                break;
+            case 'D':
+                record.push(record.at(-1) * 2);
+                break;
+            case 'C':
+                record.pop();
+                break;
+            default:
+                record.push(parseInt(op));
         }
     }
     
     return record.reduce((total, current) => total + current, 0);
+    
+    
+//     let record = [];
+    
+//     for(let i = 0; i < operations.length; i++) {
+//         if(!isNaN(parseInt(operations[i]))) {
+//             record.push(parseInt(operations[i]));
+//         } else if (operations[i] === '+') {
+//             let sum = record[record.length - 1] + record[record.length - 2];
+//             record.push(sum);
+//         } else if (operations[i] === 'D') {
+//             record.push(record[record.length - 1] * 2);
+//         } else {
+//             record.pop();
+//         }
+//     }
+    
+//     return record.reduce((total, current) => total + current, 0);
 };
