a679d765d6b95a6ce9354f2c2e34b4680b7fdb6b
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-11 18:04:04 -0400

    Time: 82 ms (64.45%), Space: 44 MB (61.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,28 +4,49 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-	if(s.length !== t.length) return false;
     
     let hash = {};
     
-      for (let i = 0; i < s.length; i++) {
-        if (hash[s[i]] !== undefined) {
-            hash[s[i]]++;
+    for(const letter of s) {
+        hash[letter] = hash[letter] + 1 || 1;
+    }
+    
+    for(const letter of t) {
+        if(hash[letter]) {
+            hash[letter]--;
         } else {
-            hash[s[i]] = 1;
+            return false;
         }
     }
     
-    console.log(hash)
-    
-    for(let j = 0; j < t.length; j++){
-        if(!hash[t[j]] || hash[t[j]] === undefined){
-            return false;
-        } else{
-            hash[t[j]]--;
-        }
+    for(const key in hash) {
+        if(hash[key] !== 0) return false;
     }
     
     return true;
+    
+// 	if(s.length !== t.length) return false;
+    
+//     let hash = {};
+    
+//       for (let i = 0; i < s.length; i++) {
+//         if (hash[s[i]] !== undefined) {
+//             hash[s[i]]++;
+//         } else {
+//             hash[s[i]] = 1;
+//         }
+//     }
+    
+//     console.log(hash)
+    
+//     for(let j = 0; j < t.length; j++){
+//         if(!hash[t[j]] || hash[t[j]] === undefined){
+//             return false;
+//         } else{
+//             hash[t[j]]--;
+//         }
+//     }
+    
+//     return true;
     
 };
