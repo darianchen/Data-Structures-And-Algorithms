3bbf38dee324584a717e6759ef519b3190200f94
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-12 15:29:46 -0400

    Time: 101 ms (85.21%), Space: 50.2 MB (73.94%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var checkValid = function(matrix) {
+    
+    // Make a hash with every number and a value of 1
+    // Iterate through all rows and cols and subtract 1
+    // If value is 0 or key doesn't exist return false
+    
+    const size = matrix.length;
+    
+    // Rows
+    for(let row = 0; row < matrix.length; row++) {
+        let hash = createHash(size);
+        for(let col = 0; col < matrix[row].length; col++) {
+            let number = matrix[row][col]
+            if(hash[number] === 0 || hash[number] === undefined) {
+                return false;
+            } else{
+                hash[number]--;
+            }
+        }
+    }
+    
+    // Cols
+        for(let col = 0; col < matrix.length; col++) {
+        let hash = createHash(size);
+        for(let row = 0; row < matrix.length; row++) {
+            let number = matrix[row][col]
+            if(hash[number] === 0 || hash[number] === undefined) {
+                return false;
+            } else{
+                hash[number]--;
+            }
+        }
+    }
+    return true;
+};
+
+function createHash(num) {
+    let hash = {};
+    for(let i = 1; i <= num; i++) {
+        hash[i] = 1;
+    }
+    return hash;
+}
