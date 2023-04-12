d16a0edc71039b08bb6a5f89fb5366019b9bf918
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-11 23:20:12 -0400

    Time: 70 ms (81.95%), Space: 44.6 MB (54.80%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -10,17 +10,16 @@ var pivotIndex = function(nums) {
     
     let sum = 0;
     
-    for(num of nums){
+    for(const num of nums) {
         sum += num;
     }
     
     let leftSum = 0;
-    let length = nums.length
     
-    for(let i = 0; i < length; i++){
-        if((sum - nums[i])/2 === leftSum) return i
-        leftSum += nums[i];
+    for(let i = 0; i < nums.length; i++) {
+        if((sum - nums[i])/2 === leftSum) return i; 
+        leftSum += nums[i];   
     }
-    
+        
     return -1;
 };
