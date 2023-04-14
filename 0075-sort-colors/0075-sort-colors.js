822fcd2c9c25033a17dedc6484596afa7f3a60f6
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-14 19:39:09 -0400

    Time: 62 ms (52.85%), Space: 41.6 MB (88.01%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,17 +3,35 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let hash = {};
     
-    for(const num of nums) {
-        hash[num] = hash[num] + 1 || 1;
-    }
+    let sorted = false;
     
-    let i = 0;
-    for(const key in hash) {
-        for(let j = 0; j < hash[key]; j++) {
-            nums[i] = key;
-            i++;
+    while(!sorted) {
+        sorted = true;
+        for(let i = 0; i < nums.length - 1; i++) {
+            if(nums[i] > nums[i + 1]) {
+                sorted = false;
+                let temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }   
         }
     }
-};
+}
+    
+    
+    
+//     let hash = {};
+    
+//     for(const num of nums) {
+//         hash[num] = hash[num] + 1 || 1;
+//     }
+    
+//     let i = 0;
+//     for(const key in hash) {
+//         for(let j = 0; j < hash[key]; j++) {
+//             nums[i] = key;
+//             i++;
+//         }
+//     }
+// };
