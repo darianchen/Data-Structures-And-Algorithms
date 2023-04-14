64d96cec0390161c31f9fcafb9fb152b73996229
Darian Chen <61950431+darianchen@users.noreply.github.com>
2023-04-14 15:36:34 -0400

    Time: 76 ms (47.43%), Space: 44.5 MB (34.95%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -6,38 +6,24 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, color) {
-    helper(image, sr, sc, color, image[sr][sc]);
-    return image
+    fill(image, sr, sc, color, image[sr][sc]);
+    return image;
 };
 
-const helper = (image, row, col, nextColor, originalColor) => {
-        
-    if(!(row >= 0 && row <= image.length - 1) || 
-       !(col >= 0 && col <= image[0].length - 1)){
-        return
-    }
+function fill(image, sr, sc, newColor, originalColor) {
+    if(sr < 0 || sr > image.length - 1 || sc < 0 || sc > image[0].length - 1) return;
     
-    if(image[row][col] !== originalColor){
-       return
-    }
+    if(image[sr][sc] !== originalColor) return;
     
-    if(image[row][col] === nextColor){
-        return
-    }
+    if(image[sr][sc] === newColor) return;
     
-    //change the color
-    //then recursion
-    
-    image[row][col] = nextColor;
-    
-    helper(image, row - 1, col, nextColor, originalColor); //up
-    helper(image, row + 1, col, nextColor, originalColor); //down
-    helper(image, row, col - 1, nextColor, originalColor); //left
-    helper(image, row, col + 1, nextColor, originalColor); //right
-    
+    image[sr][sc] = newColor;
+    fill(image, sr - 1, sc, newColor, originalColor);
+    fill(image, sr + 1, sc, newColor, originalColor);
+    fill(image, sr, sc - 1, newColor, originalColor);
+    fill(image, sr, sc + 1, newColor, originalColor);
 }
-    
-    //basecase if out of bounds or not number you clicked on ex) 1
-    // check if number already changed
+
+
     
     
